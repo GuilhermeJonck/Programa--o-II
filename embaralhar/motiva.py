88a970f5759932e5f90adc71e0a77be9dd8614bf
guilherme jonck <guijonckk@gmail.comgit config user.email guijonckk@gmail.com>
2022-09-06 11:10:07 -0300

    Criação da main

--- embaralhar/motiva.py
+++ embaralhar/motiva.py
@@ -1,10 +1,8 @@
 from random import choice
 def motivacional():
-    frases={
-            1:"Não desista!!!",
-            2:"Quase lá,só mais um pouco",
-            3:"Você está chegando perto!!!",
-            4:"Tente de novo,vamos lá!!!",
-            5:"Aguente firme!!",
-        }
+    frases=("Não desista!!!",
+            "Quase lá,só mais um pouco",
+            "Você está chegando perto!!!",
+            "Tente de novo,vamos lá!!!","Aguente firme!!")
+    
     return choice(frases)
